c5cf6dfb883621ed983ae502c59fa74318ce4dbb
Exekiel2k3 <sebastian.scardigno@gmail.com>
2017-05-07 18:54:49 -0300

    Create Solution.java

---
+++ Java/Exception Handling/Java Exception Handling -Try-catch/Solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        
+        try {
+            System.out.println((sc.nextInt()/sc.nextInt()));
+        } catch (InputMismatchException e) {
+            System.out.println(e.getClass().getCanonicalName());
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+    }
+}
