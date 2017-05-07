4d406e267a8b85b8e1beafae1919209b53077c0a
Exekiel2k3 <sebastian.scardigno@gmail.com>
2017-05-07 18:10:48 -0300

    Create test.java

---
+++ Java/Data Structures/Java Dequeue/test.java
@@ -0,0 +1,25 @@
+    import java.util.*;
+    public class test {
+        public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Deque<Integer> deque = new ArrayDeque<>();
+        int n = in.nextInt();
+        int m = in.nextInt();
+        HashSet<Integer> set = new HashSet<>();
+        int max = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < n; i++) {
+            int num = in.nextInt();
+            deque.add(num);
+            set.add(num);
+            
+            if (deque.size() == m) {
+                if (set.size() > max) max = set.size();
+                int first = deque.remove();
+                if (!deque.contains(first)) set.remove(first);
+            }
+            
+        }
+        System.out.println(max);
+    }
+}
